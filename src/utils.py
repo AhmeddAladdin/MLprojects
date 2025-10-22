720a51067a2b3829397889972b260b9e5aedc2e9
Ahmed Aladdin <ahmeddaladdin@gmail.com>
2025-10-22 22:44:03 +0300

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -17,12 +18,17 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+            gscv = GridSearchCV(model, para, cv=3, n_jobs=3)
+            gscv.fit(X_train, y_train)
+
+            model.set_params(**gscv.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
@@ -31,9 +37,9 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[list(models.keys())[i]] = (test_model_score, gscv.best_params_)
 
         return report
 
-    except:
-        pass
+    except Exception as e:
+        raise CustomException(e,sys)
